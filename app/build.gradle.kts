7cbf06f4e603b65f8fe7a71f5b50129972092bdb
nono2359 <nono.world.2359@gmail.com>
2026-08-09 17:49:27 +0900

    chore: release v4.0.1-jp.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ hilt {
     enableAggregatingTask = true
 }
 
-val appVersionCode = 40103
-val appVersionName = "4.0.1-jp.3"
+val appVersionCode = 40104
+val appVersionName = "4.0.1-jp.4"
 val appId = "jp.nono2359.pcrtool"
 val releaseStoreFile = providers.environmentVariable("PCR_RELEASE_STORE_FILE")
 val releaseStorePassword = providers.environmentVariable("PCR_RELEASE_STORE_PASSWORD")
